ad170fa6ae81c96f69b28a96a37b0d2faaacd81e
Yukhosmith <yuskhosmith1@gmail.com>
2023-04-09 20:22:35 +0100

    Monsters-Rolodex

---
+++ src/components/card-container/card-container.component.jsx
@@ -0,0 +1,30 @@
+// import { Component } from "react";
+
+import './card-container.styles.css'
+
+const CardContainer = ({monster}) => {
+    const {name, email, id} = monster;
+    return(
+        <div className="card-container" key={id}>
+            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    );
+}
+
+// Class Component
+// class CardContainer extends Component {
+//     render(){
+//         const {name, email, id} = this.props.monster;
+//         return(
+//             <div className="card-container" key={id}>
+//                 <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+//                 <h2>{name}</h2>
+//                 <p>{email}</p>
+//             </div>
+//         );
+//     }
+// }
+
+export default CardContainer;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,33 @@
+// import { Component } from "react";
+
+import './card-list.styles.css'
+
+import CardContainer from '../card-container/card-container.component'
+
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map(monster => {
+            return <CardContainer monster={monster}/>;
+        })}
+    </div>
+);
+
+
+// Class Component
+
+
+// class CardList extends Component {
+//     render() {
+//         const {monsters} = this.props;
+        
+//         return (
+//             <div className="card-list">
+//                 {monsters.map(monster => {
+//                     return <CardContainer monster={monster}/>;
+//                 })}
+//             </div>
+//         );
+//     }
+// }
+
+export default CardList;
